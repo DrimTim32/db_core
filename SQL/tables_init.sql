80ba0225da90983d498a366b98ad86acc7bb97d8
Malin <szyszka45@gmail.com>
2016-12-11 21:09:44 +0100

    added users and employees tables + procedures

--- SQL/tables_init.sql
+++ SQL/tables_init.sql
@@ -9,3 +9,16 @@ INSERT INTO Units (unit_name, convert_factor, unit_type) VALUES
   ('dekagram', 100, 2),
   ('litr', 1, 1),
   ('mililitr', 1000, 1)
+-- dodawanie administratora 
+EXEC	[dbo].[AddEmployee]
+		@name = N'Marcin',
+		@surname = N'Malinowski',
+		@phone = N'+48123123123',
+		@position = 255
+		 
+
+exec  [dbo].[AddUserByInfo]
+		@password = N'qwerty',
+		@username = N'malin',
+		@name = 'Marcin',
+		@surname = 'Malinowski'
